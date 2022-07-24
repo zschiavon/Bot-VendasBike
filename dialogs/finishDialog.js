b6b28852a6b3643eb901aed2962ad863c938ed91
ramonrocha1989 <ramon.rocha.pb@compasso.com.br>
2022-07-24 15:05:53 -0300

    update files

--- dialogs/finishDialog.js
+++ dialogs/finishDialog.js
@@ -2,27 +2,29 @@ const { MessageFactory } = require("botbuilder");
 const { TextPrompt, WaterfallDialog, ChoicePrompt, ChoiceFactory } = require("botbuilder-dialogs");
 const { CancelAndHelpDialog } = require("./cancelAndHelpDialog");
 
-const TEXT_PROMPT  = 'textPrompt'
-const CHOICE_PROMPT  = 'choicePrompt'
-const WATERFALL_DIALOG  = 'waterfallDialog'
+const TEXT_PROMPT = 'textPrompt'
+const CHOICE_PROMPT = 'choicePrompt'
+const WATERFALL_DIALOG = 'waterfallDialog'
 
-class FinishDialog extends CancelAndHelpDialog{
-    constructor(id){
+class FinishDialog extends CancelAndHelpDialog {
+    constructor(id, luisRecognizer) {
         super(id || 'finishDialog')
 
+        this.luisRecognizer = luisRecognizer;
         this.addDialog(new TextPrompt(TEXT_PROMPT))
             .addDialog(new ChoicePrompt(CHOICE_PROMPT))
-            .addDialog(new WaterfallDialog(WATERFALL_DIALOG),[
+            .addDialog(new WaterfallDialog(WATERFALL_DIALOG), [
                 this.avaliationStep.bind(this),
-                this.finalStep.bind()
+                this.finalStep.bind(),
+                this.feedbackStep.bind()
             ])
 
         this.initialDialogId = WATERFALL_DIALOG;
     }
 
-    async avaliationStep(stepContext){
+    async avaliationStep(stepContext) {
         const firstMessage = 'Antes de encerrar, eu gostaria de saber se foi tudo bem na nossa conversa ou se em algum momento o meu pneu furou...'
-        await stepContext.sendActivity(firstMessage)            
+        await stepContext.sendActivity(firstMessage)
 
         return await stepContext.prompt(CHOICE_PROMPT, {
             prompt: 'Como avalia meu atendimento?',
@@ -32,33 +34,33 @@ class FinishDialog extends CancelAndHelpDialog{
                 `Nem amei, nem odiei ${String.fromCodePoint(0x1F610)}`,
                 `Gostei ${String.fromCodePoint(0x1F603)}`,
                 `Amei Muito ${String.fromCodePoint(0x2764)}`
-        ])
+            ])
         });
     }
 
-    async finalStep(stepContext){
-        const avaliation = [stepContext.result.index]        
-         const result = avaliation.some(a => a < 3)
+    async finalStep(stepContext) {
+        const avaliation = [stepContext.result.index]
+        const result = avaliation.some(a => a < 3)
 
-        if(result){
+        if (result) {
             return await stepContext.prompt(TEXT_PROMPT, `Que pena! ${String.fromCodePoint(0x1F61E)} 
             Lamento não ter atingido suas expectativas! Como meu atendimento poderia ser melhor?`)
-            
-        }else{
-            const thankMessage = `Muito obrigado pela avaliação! ${String.fromCodePoint(0x1F44D)}\n Adorei falar com você!`                   
-            const byeMessage = `Qualquer dúvida para comprar sua bike, estou sempre por aqui`                   
+
+        } else {
+            const thankMessage = `Muito obrigado pela avaliação! ${String.fromCodePoint(0x1F44D)}\n Adorei falar com você!`
+            const byeMessage = `Qualquer dúvida para comprar sua bike, estou sempre por aqui`
             await stepContext.context.sendActivity(thankMessage)
             await stepContext.context.sendActivity(byeMessage)
             return await stepContext.cancelAllDialogs()
-            
-         }
+
+        }
     }
-    async feedbackStep(stepContext){
+    async feedbackStep(stepContext) {
         const feedback = stepContext.result
         // Enviar o feedback para algum lugar
         const byeMessage = 'Obrigado pelo feedback, isso vai ajudar a me tornar um assistente virtual melhor. Até a próxima!'
         await stepContext.context.sendActivity(byeMessage)
-        return await stepContext.cancelAllDialogs()        
+        return await stepContext.cancelAllDialogs()
 
     }
 }
