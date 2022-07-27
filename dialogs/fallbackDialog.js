cce81ba1005bf94402e55401ed0357fd59fcfc62
Isadora Brito <isadoradpbrito@gmail.com>
2022-07-27 03:26:20 -0300

    adiciona os steps restantes no fallback

--- dialogs/fallbackDialog.js
+++ dialogs/fallbackDialog.js
@@ -1,3 +1,4 @@
+const { MessageFactory } = require('botbuilder');
 const { LuisRecognizer } = require('botbuilder-ai');
 const { ConfirmPrompt, TextPrompt, WaterfallDialog } = require('botbuilder-dialogs');
 const { CancelAndHelpDialog } = require('./cancelAndHelpDialog');
@@ -15,31 +16,41 @@ class FallbackDialog extends CancelAndHelpDialog {
             .addDialog(new WaterfallDialog(WATERFALL_DIALOG, [
                 this.firstStep.bind(this),
                 this.secondStep.bind(this),
-                this.thirdStep.bind(this)
+                this.thirdStep.bind(this),
+                this.fourthStep.bind(this)
             ]));
 
         this.initialDialogId = WATERFALL_DIALOG;
     }
 
     async firstStep(stepContext) {
-        const Message = 'Ihhh, parece que o pneu furou... Estou com dificuldades para entender! Você poderia repetir com outras palavras? FUNCIONOUUUU';
+        const Message = 'Ihhh, parece que o pneu furou... Estou com dificuldades para entender! Você poderia repetir com outras palavras?';
         await stepContext.context.sendActivity(Message);
         return await stepContext.prompt(TEXT_PROMPT, '');
     }
 
-    // TODO: Direcionar para step correto do fluxo. ONPROMPT
     async secondStep(stepContext) {
         switch (LuisRecognizer.topIntent(stepContext.context.luisResult)) {
-        case 'FiltroCor': return await stepContext.continueDialog('colorDialog');
-        case 'FiltroTipo': return await stepContext.beginDialog('typeDialog');
-        case 'FiltroPreco': return await stepContext.beginDialog('priceDialog');
-        case 'FiltroGenero': return await stepContext.beginDialog('genderDialog');
+        case 'FiltroCor': return await stepContext.replaceDialog('colorDialog', { bikeVector: stepContext.values.bikeVector, last: stepContext.values.last });
+        case 'FiltroTipo': return await stepContext.replaceDialog('typeDialog', { bikeVector: stepContext.values.bikeVector, last: stepContext.values.last });
+        case 'FiltroPreco': return await stepContext.replaceDialog('priceDialog', { bikeVector: stepContext.values.bikeVector, last: stepContext.values.last });
+        case 'FiltroGenero': return await stepContext.replaceDialog('genderDialog', { bikeVector: stepContext.values.bikeVector, last: stepContext.values.last });
         default: return await stepContext.next();
         }
     }
 
     async thirdStep(stepContext) {
+        const Message = 'Sigo sem entender. Você pode ir para o menu inicial para facilitar, ou então encerrar nossa conversa.';
+        await stepContext.context.sendActivity(Message);
+        return await stepContext.prompt(TEXT_PROMPT, MessageFactory.suggestedActions(['Menu', 'Encerrar']));
+    }
 
+    async fourthStep(stepContext) {
+        if (LuisRecognizer.topIntent(stepContext.context.luisResult) != 'Menu') {
+            const Message = 'Sinto muito, ainda estou aprendendo e no momento não consigo entender o que você deseja. Mas podemos tentar conversar novamente mais tarde!';
+            await stepContext.context.sendActivity(Message);
+            return await stepContext.cancelAllDialogs();
+        }
     }
 }
 module.exports.FallbackDialog = FallbackDialog;
